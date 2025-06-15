37cb3ca4c7c5e43eed3626f6fbbb7b24b1436553
OpchetZ <varanyookasamsuk@gmail.com>
2025-06-15 22:02:02 +0700

    learn end

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-
-
+const exhdb = require('express-handlebars');
+const users = require('./Users')
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -14,9 +14,18 @@ app.use('/api/users', require('./routes/api/users'))
 
 //middleware
 app.use(logger);
- 
+
+app.engine('handlebars', exhdb.engine({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
 
 
+//homepage
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'User',
+        users
+});
+})
 // app.get('/', (req,res) => {
 //     res.sendFile(path.join(__dirname, 'public','index.html'));
 // })
--- routes/api/users.js
+++ routes/api/users.js
@@ -23,6 +23,35 @@ router.post('/', (req,res) =>{
    if (!newUser.name || !newUser.email) {
         return res.status(400).json({msg: 'please include name and email'})
    }
+   users.push(newUser);
+   res.redirect('/');
+})
+router.put('/:id', (req, res) =>{
+        let found = users.some(user => user.id === parseInt(req.params.id));
+        if (found) {
+            const updUser = req.body;
+            users.forEach(user => {
+                if (user.id === parseInt(req.params.id)) {
+                    user.name = updUser ? updUser.name : user.name;
+                    user.email = updUser ? updUser.email : user.email;
+                    res.json({ msg: 'User update',user});
+                }
+            })
+        }else{
+            res.status(400).json({ msg: `NO user with the id ${req.params.id}`})
+        }
 })
 
+router.delete('/:id', (req, res) => {
+            let found = users.some(user => user.id === parseInt(req.params.id));
+    if (found) {
+        res.json({
+            msg: 'DELETED',
+            users: users.filter(user => user.id !== parseInt(req.params.id))
+
+        })
+    }else{
+        res.status(400).json({ msg: `No id ${req.params.id}`});
+    }
+})
 module.exports = router;
